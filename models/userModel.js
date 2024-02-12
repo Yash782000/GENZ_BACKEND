6b864ed8ef1b864a8f0749abaa54d1139308d545
yash barot <yashbarot@yashs-air.home>
2024-02-11 20:53:25 -0500

    backend Part

--- models/userModel.js
+++ models/userModel.js
@@ -1,82 +1,60 @@
 const mongoose = require("mongoose");
-const bcrypt = require("bcryptjs");
-const { ObjectId } = mongoose.Schema;
+const bcrypt = require("bcryptjs")
+const {objectId} = mongoose.Schema;
 
-const userSchema = mongoose.Schema(
-  {
-    name: {
-      type: String,
-      required: [true, "Please add a name"],
+
+const userSchema = mongoose.Schema({
+    name:{
+        type:String,
+        required:[true,"Please add a name"]
     },
     email: {
-      type: String,
-      required: [true, "Please add a email"],
-      unique: true,
-      trim: true,
-      match: [
-        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-        "Please enter a valid emaial",
-      ],
+        type: String,
+        trim: true,
+        lowercase: true,
+        unique: true,
+        required: 'Email address is required',
+       // validate: [validateEmail, 'Please fill a valid email address'],
+        match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']
     },
-    password: {
-      type: String,
-      required: [true, "Please add a password"],
-      minLength: [6, "Password must be up to 6 characters"],
-      //   maxLength: [23, "Password must not be more than 23 characters"],
+    password:{
+        type:String,
+        required:[true,"Please add a password"],
+        minLength:[6,"Password must be up to 6 characters"]
     },
-    role: {
-      type: String,
-      required: [true],
-      default: "customer",
-      enum: ["customer", "admin"],
+    role:{
+        type:String,
+        required:true,
+        default:"customer",
+        enum:["customer","admin"]
     },
-    photo: {
-      type: String,
-      required: [true, "Please add a photo"],
-      default: "https://i.ibb.co/4pDNDk1/avatar.png",
+    photo:{
+        type:String,
+        required:[true,"please add a photo"],
+        default:"https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.istockphoto.com%2Fphotos%2Fuser-profile&psig=AOvVaw2uzrA3Ie3AP-5K66ax3aPz&ust=1707326215367000&source=images&cd=vfe&opi=89978449&ved=0CBMQjRxqFwoTCPD6n_ibl4QDFQAAAAAdAAAAABAE"
     },
-    phone: {
-      type: String,
-      default: "+234",
+    phone:{
+        type:String,
+        default:"+91"
     },
-    address: {
-      type: Object,
-      // address, state, country
-    },
-    wishlist: [{ type: ObjectId, ref: "Product" }],
-    balance: {
-      type: Number,
-      default: 0,
-    },
-    cartItems: {
-      type: [Object],
-    },
-    isVerified: {
-      type: Boolean,
-      default: false,
-    },
-    stripeCustomerId: {
-      type: String,
-      // required: true,
-    },
-  },
-  {
-    timestamps: true,
-  }
-);
+    address:{
+        type:Object,
+        //address,state,country
+    }
+})
+userSchema.pre("save",async function(next){
+    if(!this.isModified("password")){
+        return next()
+    }
+    else{
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword =await  bcrypt.hash(this.password,salt);
+        this.password = hashedPassword;
+        next();
+    }
+})
 
-//   Encrypt password before saving to DB
-userSchema.pre("save", async function (next) {
-  if (!this.isModified("password")) {
-    return next();
-  }
 
-  // Hash password
-  const salt = await bcrypt.genSalt(10);
-  const hashedPassword = await bcrypt.hash(this.password, salt);
-  this.password = hashedPassword;
-  next();
-});
+const User = mongoose.model("User",userSchema);
 
-const User = mongoose.model("User", userSchema);
 module.exports = User;
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,45 +1,14 @@
 const express = require("express");
 const router = express.Router();
-const {
-  registerUser,
-  loginUser,
-  logout,
-  getUser,
-  getLoginStatus,
-  updateUser,
-  addToWishlist,
-  removeFromWishlist,
-  getWishlist,
-  getCart,
-  saveCart,
-  clearCart,
-  updatePhoto,
-  // changePassword,
-  // forgotPassword,
-  // resetPassword,
-} = require("../controllers/userController");
-const { protect } = require("../middleware/authMiddleware");
-// comment
+const { registerUser, loginUser, logOut,getUser, getLoginStatus, updateUser, updatePhoto } = require("../controllers/userController");
+const {protect} = require("../middleware/authMiddleware")
 
-router.post("/register", registerUser);
-router.post("/login", loginUser);
-router.get("/logout", logout);
-router.get("/getUser", protect, getUser);
-router.get("/getLoginStatus", getLoginStatus);
-router.patch("/updateUser", protect, updateUser);
-router.patch("/updatePhoto", protect, updatePhoto);
-// router.patch("/changepassword", protect, changePassword);
-// router.post("/forgotpassword", forgotPassword);
-// router.put("/resetpassword/:resetToken", resetPassword);
-
-// wishlist
-router.post("/addToWishlist", protect, addToWishlist);
-router.get("/getWishlist", protect, getWishlist);
-router.put("/wishlist/:productId", protect, removeFromWishlist);
-
-// cart
-router.get("/getCart", protect, getCart);
-router.patch("/saveCart", protect, saveCart);
-router.patch("/clearCart", protect, clearCart);
-
+// Define route handlers
+router.post("/register", registerUser); // Assuming user registration is handled at the root path ("/")
+router.post("/login",loginUser)
+router.get("/logout",logOut)
+router.get("/getUser",protect,getUser);
+router.get("/getLoginStatus",getLoginStatus);
+router.patch("/updateUser",protect,updateUser)
+router.patch("/updatePhoto",protect,updatePhoto)
 module.exports = router;
